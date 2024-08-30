ab34aa3fc7efd37a202ed0cc5b58070f05d34006
Maojin-juan <edwardelric199887@gmail.com>
2024-08-30 15:31:15 +0800

    安裝shadcn：dropdownmenu、tabs

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,17 +1,28 @@
 import Image from "next/image";
 import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
   MdKeyboardArrowDown,
   MdKeyboardArrowLeft,
   MdKeyboardArrowRight,
   MdOutlineSearch,
   MdOutlineShoppingCart,
   MdOutlineSettings,
+  MdOutlineStar,
 } from "react-icons/md";
 import { LiaBellSolid } from "react-icons/lia";
 import { BsList, BsBookmarkFill } from "react-icons/bs";
 import { CgProfile } from "react-icons/cg";
 import { RiFileList2Line, RiPencilFill } from "react-icons/ri";
 import { TbLogout } from "react-icons/tb";
+import { AiOutlineNotification } from "react-icons/ai";
 
 const menu = [
   { title: "音樂", link: "#" },
@@ -56,7 +67,19 @@ const Navbar = () => {
               />
             </h1>
           </a>
-          <div className="group relative hidden lg:block">
+          <DropdownMenu>
+            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          {/* <div className="group relative hidden xl:block">
             <a
               className="flex items-center gap-x-1 px-6 py-5 text-[#4B4B4B]"
               href="#"
@@ -64,12 +87,12 @@ const Navbar = () => {
               <span className="whitespace-nowrap pe-1">探索</span>
               <MdKeyboardArrowDown size={20} color="#909090" />
             </a>
-            <div className="absolute -left-12 hidden pt-6 group-hover:block">
-              <div className="rounded-s-xl bg-[#F9F9F9] px-3 pt-3">
+            <div className="absolute -left-12 hidden h-[624px] overflow-y-hidden pt-6 group-hover:flex">
+              <div className="rounded-s-xl bg-[#F9F9F9] px-3 pb-1 pt-3">
                 <p className="mb-2 py-3 ps-6">探索全部</p>
                 <ul>
                   {menu.map((item, index) => (
-                    <li className="rounded-xl" key={item.title}>
+                    <li key={`${item.title}${index}`}>
                       <a
                         href="#"
                         className="group/subItem flex min-w-52 items-center justify-between rounded-xl py-3 pe-3 ps-6 hover:bg-[#0068FF] hover:font-semibold hover:text-white"
@@ -80,32 +103,175 @@ const Navbar = () => {
                           size={24}
                         />
                       </a>
-                      <div className="absolute left-full top-0 mt-6 border-x border-[#ECECEC] bg-white px-3 pt-3">
-                        <p className="mb-2 py-3 ps-6">探索全部</p>
-                        <ul>
-                          {item.title &&
-                            menu_program.map((subItem, subIndex) => (
-                              <li key={subItem.title}>
-                                <a
-                                  href="#"
-                                  className="group/subSubItem flex min-w-52 items-center justify-between rounded-xl py-3 pe-3 ps-6 hover:bg-[#0068FF] hover:font-semibold hover:text-white"
-                                >
-                                  {subItem.title}
-                                  <MdKeyboardArrowRight
-                                    className="text-[#909090] group-hover/subSubItem:text-white"
-                                    size={24}
-                                  />
-                                </a>
-                              </li>
-                            ))}
-                        </ul>
-                      </div>
                     </li>
                   ))}
                 </ul>
               </div>
+              <div className="border-x border-[#ECECEC] bg-white px-3 pb-1 pt-3">
+                <p className="mb-2 py-3 ps-6">探索全部</p>
+                <ul>
+                  {menu_program.map((subItem, subIndex) => (
+                    <li key={subItem.title}>
+                      <a
+                        href="#"
+                        className="group/subSubItem flex min-w-52 items-center justify-between rounded-xl py-3 pe-3 ps-6 hover:bg-[#0068FF] hover:font-semibold hover:text-white"
+                      >
+                        {subItem.title}
+                        <MdKeyboardArrowRight
+                          className="text-[#909090] group-hover/subSubItem:text-white"
+                          size={24}
+                        />
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="min-w-[399px] rounded-e-xl bg-white px-3 pb-1 pt-3">
+                <div className="flex items-center gap-x-2 pb-4">
+                  <Image
+                    src="https://github.com/hexschool/2022-web-layout-training/blob/main/week8-learning/icons/ic_fire.png?raw=true"
+                    alt="fire"
+                    width={24}
+                    height={24}
+                  ></Image>
+                  <p className="whitespace-nowrap font-semibold">
+                    熱門網頁前端課程
+                  </p>
+                </div>
+                <ul>
+                  <li className="flex w-full gap-x-2 border-t border-[#ECECEC] pb-6 pt-4">
+                    <div className="relative after:absolute after:left-1 after:top-1 after:rounded-full after:bg-[#FFE0D7] after:px-2 after:py-1 after:tracking-[0.2rem] after:text-[#FF3A55] after:content-['8折']">
+                      <Image
+                        src="https://github.com/hexschool/2022-web-layout-training/blob/main/week8-learning/course-5.png?raw=true"
+                        alt="course"
+                        width={100}
+                        height={100}
+                        className="relative flex size-[100px] self-start rounded-xl object-cover"
+                      ></Image>
+                    </div>
+                    <div>
+                      <a href="#" className="mb-4 font-semibold">
+                        小白也會！從零開始學習 Python 程式設計
+                      </a>
+                      <div className="flex items-center justify-between whitespace-nowrap border-b border-[#ECECEC] pb-2">
+                        <div className="flex items-center gap-x-2">
+                          <Image
+                            src="https://github.com/hexschool/2022-web-layout-training/blob/main/week8-learning/avatar-2.png?raw=true"
+                            alt="teacherAvatar"
+                            width={32}
+                            height={32}
+                          ></Image>
+                          <p className="text-[#4B4B4B]">Kelly Hsu</p>
+                        </div>
+                        <div className="flex flex-col items-end">
+                          <p className="font-semibold text-[#0068FF]">
+                            NT$ 2,480
+                          </p>
+                          <del className="text-xs text-[#808080]">
+                            NT$ 3,200
+                          </del>
+                        </div>
+                      </div>
+                      <div className="flex justify-between py-2">
+                        <p className="text-[#4B4B4B]">已有 420 位同學加入</p>
+                        <div className="flex items-center gap-x-1">
+                          <MdOutlineStar color="#F6BD2B" size={12} />
+                          <p>4.0</p>
+                          <p className="text-[#808080]">(333)</p>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="flex w-full gap-x-2 border-t border-[#ECECEC] pb-6 pt-4">
+                    <div className="relative after:absolute after:left-1 after:top-1 after:rounded-full after:bg-[#FFE0D7] after:px-2 after:py-1 after:tracking-[0.2rem] after:text-[#FF3A55] after:content-['8折']">
+                      <Image
+                        src="https://github.com/hexschool/2022-web-layout-training/blob/main/week8-learning/course-5.png?raw=true"
+                        alt="course"
+                        width={100}
+                        height={100}
+                        className="relative flex size-[100px] self-start rounded-xl object-cover"
+                      ></Image>
+                    </div>
+                    <div>
+                      <a href="#" className="mb-4 font-semibold">
+                        小白也會！從零開始學習 Python 程式設計
+                      </a>
+                      <div className="flex items-center justify-between whitespace-nowrap border-b border-[#ECECEC] pb-2">
+                        <div className="flex items-center gap-x-2">
+                          <Image
+                            src="https://github.com/hexschool/2022-web-layout-training/blob/main/week8-learning/avatar-2.png?raw=true"
+                            alt="teacherAvatar"
+                            width={32}
+                            height={32}
+                          ></Image>
+                          <p className="text-[#4B4B4B]">Kelly Hsu</p>
+                        </div>
+                        <div className="flex flex-col items-end">
+                          <p className="font-semibold text-[#0068FF]">
+                            NT$ 2,480
+                          </p>
+                          <del className="text-xs text-[#808080]">
+                            NT$ 3,200
+                          </del>
+                        </div>
+                      </div>
+                      <div className="flex justify-between py-2">
+                        <p className="text-[#4B4B4B]">已有 420 位同學加入</p>
+                        <div className="flex items-center gap-x-1">
+                          <MdOutlineStar color="#F6BD2B" size={12} />
+                          <p>4.0</p>
+                          <p className="text-[#808080]">(333)</p>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="flex w-full gap-x-2 border-t border-[#ECECEC] pb-6 pt-4">
+                    <div className="relative after:absolute after:left-1 after:top-1 after:rounded-full after:bg-[#FFE0D7] after:px-2 after:py-1 after:tracking-[0.2rem] after:text-[#FF3A55] after:content-['8折']">
+                      <Image
+                        src="https://github.com/hexschool/2022-web-layout-training/blob/main/week8-learning/course-5.png?raw=true"
+                        alt="course"
+                        width={100}
+                        height={100}
+                        className="relative flex size-[100px] self-start rounded-xl object-cover"
+                      ></Image>
+                    </div>
+                    <div>
+                      <a href="#" className="mb-4 font-semibold">
+                        小白也會！從零開始學習 Python 程式設計
+                      </a>
+                      <div className="flex items-center justify-between whitespace-nowrap border-b border-[#ECECEC] pb-2">
+                        <div className="flex items-center gap-x-2">
+                          <Image
+                            src="https://github.com/hexschool/2022-web-layout-training/blob/main/week8-learning/avatar-2.png?raw=true"
+                            alt="teacherAvatar"
+                            width={32}
+                            height={32}
+                          ></Image>
+                          <p className="text-[#4B4B4B]">Kelly Hsu</p>
+                        </div>
+                        <div className="flex flex-col items-end">
+                          <p className="font-semibold text-[#0068FF]">
+                            NT$ 2,480
+                          </p>
+                          <del className="text-xs text-[#808080]">
+                            NT$ 3,200
+                          </del>
+                        </div>
+                      </div>
+                      <div className="flex justify-between py-2">
+                        <p className="text-[#4B4B4B]">已有 420 位同學加入</p>
+                        <div className="flex items-center gap-x-1">
+                          <MdOutlineStar color="#F6BD2B" size={12} />
+                          <p>4.0</p>
+                          <p className="text-[#808080]">(333)</p>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
+          </div> */}
           <div className="hidden items-center justify-between gap-x-4 rounded-2xl px-2 outline outline-1 outline-[#ECECEC] lg:flex lg:max-w-[425px] lg:flex-1">
             <label
               className="flex w-full items-center gap-x-2"
@@ -127,14 +293,29 @@ const Navbar = () => {
             </button>
           </div>
           <div className="ms-auto flex items-center md:gap-x-4">
-            <a
-              className="relative before:absolute before:right-2 before:top-1 before:size-2 before:rounded-full before:bg-[#0068FF] before:content-['']"
-              href="#"
-            >
-              <LiaBellSolid className="m-2 mx-3" size={24} />
-            </a>
-            <a className="relative" href="#">
-              <MdOutlineShoppingCart className="m-2 mx-3" size={24} />
+            <div className="group relative">
+              <a
+                className="relative block rounded-xl px-3 py-2 before:absolute before:right-2 before:top-1 before:size-2 before:rounded-full before:bg-[#0068FF] before:content-[''] hover:bg-[#F9F9F9]"
+                href="#"
+              >
+                <LiaBellSolid size={24} />
+              </a>
+              <div className="absolute hidden overflow-y-hidden pt-6 group-hover:flex">
+                <ul>
+                  <li>
+                    <AiOutlineNotification />
+                    <div>
+                      <p>系統通知：6 月 30 日 02:00 網站系統排程更新</p>
+                      <p>4 天前</p>
+                    </div>
+                  </li>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </div>
+            </div>
+            <a className="relative block px-3 py-2" href="#">
+              <MdOutlineShoppingCart size={24} />
               <span className="absolute right-1.5 top-0.5 flex size-5 items-center justify-center rounded-full border-2 border-white bg-[#0068FF] text-xs text-white">
                 2
               </span>
@@ -168,7 +349,7 @@ const Navbar = () => {
           <p className="mb-2 py-3 ps-6">探索全部</p>
           <ul>
             {menu.map((item, index) => (
-              <li key={item.title}>
+              <li key={`${item.title}${index}`}>
                 <a
                   href="#"
                   className="group flex items-center justify-between rounded-xl py-3 pe-3 ps-6 hover:bg-[#0068FF] hover:font-semibold hover:text-white"
